40d9aaa5a155096d961dcbb0bdfdbb43802e3390
Michal Nazarewicz <mina86@mina86.com>
2024-03-07 15:25:57 +0100

    Introduce Verifier with interface for checking Ed25519 signatures

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,9 @@ pub mod ed25519_program;
 #[cfg(not(feature = "library"))]
 mod program;
 mod stdx;
+#[cfg(feature = "library")]
+mod verifier;
 
 pub use api::{SignatureHash, SignaturesAccount};
+#[cfg(feature = "library")]
+pub use verifier::Verifier;
